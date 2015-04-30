17fc4037335c8555cc3602012712de80c9fe9850
CJ Griscom <cjgriscom@comcast.net>
2015-04-29 20:17:13 -0500

    Add type vector request methods to Call

--- src/Call.h
+++ src/Call.h
@@ -20,6 +20,24 @@ public:
 
 	uint32_t lineN;
 
+	vector<Datatype> input_types() {
+		vector<Datatype> typeList;
+		for (VarReference vr : inputs) typeList.push_back(vr.datatype());
+		return typeList;
+	}
+
+	vector<Datatype> confNode_types() {
+		vector<Datatype> typeList;
+		for (ConfNode cn : confNodes) typeList.push_back(cn.type);
+		return typeList;
+	}
+
+	vector<Datatype> output_types() {
+		vector<Datatype> typeList;
+		for (VarReference vr : inputs) typeList.push_back(vr.datatype());
+		return typeList;
+	}
+
 };
 
 #endif /* CALL_H_ */
