49e6fc8221839a823449a48effb88a81abec783b
CJ Griscom <cjgriscom@comcast.net>
2015-04-30 19:34:25 -0500

    Add true and false to StdLib

--- src/StdLib.h
+++ src/StdLib.h
@@ -6,6 +6,7 @@
 #include <OthUtil.h>
 #include <OthFile.h>
 #include <Function.h>
+#include <Datatypes.h>
 #include <CallParser.cpp>
 #include <FunctionBuilder.cpp>
 
@@ -19,6 +20,7 @@ inline void define_std() {
 
 	const string NATIVES = R"(
 simple sequence [a:anything]:[b:typeof(a)]
+simple sequence [c:boolean, a:anything, b:typeof(a)]?:[r:typeof(a)]
 
 simple sequence [a:numeric]as{type:DATATYPE}[b:node(type)]
 
@@ -38,12 +40,14 @@ simple sequence [a:boolean,b:boolean]&[c:boolean]
 simple sequence [a:boolean,b:boolean]|[c:boolean]
 simple sequence [a:boolean,b:boolean]^[c:boolean]
 simple sequence [a:boolean]![b:boolean]
+
+simple sequence [a:integer,b:typeof(a)]&[c:typeof(a)]
+simple sequence [a:integer,b:typeof(a)]|[c:typeof(a)]
+simple sequence [a:integer,b:typeof(a)]^[c:typeof(a)]
+simple sequence [a:integer]![b:typeof(a)]
 	)";
 
-	int index = 0;
-	while (index == 0){
-		addLine(++index, trim(NATIVES));
-	}
+	addLine(1, trim(NATIVES));
 
 	parse();
 	assembleFile(STANDARD_LIB, finalCalls);
@@ -52,7 +56,15 @@ simple sequence [a:boolean]![b:boolean]
 		f.resolved = true;
 	}
 
-}
+	STANDARD_LIB.constants.push_back("_false");
+	STANDARD_LIB.constant_types.push_back(Datatype(BOOL));
+	STANDARD_LIB.constant_values.push_back("0");
+	STANDARD_LIB.constant_lines.push_back(0);
+	STANDARD_LIB.constants.push_back("_true");
+	STANDARD_LIB.constant_types.push_back(Datatype(BOOL));
+	STANDARD_LIB.constant_values.push_back("1");
+	STANDARD_LIB.constant_lines.push_back(0);
 
+}
 
 #endif
